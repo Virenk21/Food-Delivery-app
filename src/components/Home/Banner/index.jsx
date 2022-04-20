c357c09c92cebb87d9cacb5bc8cc6fcd5f2f9685
Virenk21 <viren.d222111@gmail.com>
2022-04-20 17:04:56 +0530

    updated menu logo

--- src/components/Home/Banner/index.jsx
+++ src/components/Home/Banner/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './style.css';
 import bannerImg from './banner.png';
+import Logo from '../../Common/Logo';
+
 
 const Banner = () => {
   return (
    <header>
-<div className='header-content'></div>
+<div className='header-content'>
+  <Logo />
+  <div className="content-main">
+
+    <h1>Delicious foor for your cravings</h1>
+    <p>We made fresh and healty meals with different receipes</p>
+    <button>View Menu<i className='fas fa-long-arrow-alt-right' ></i></button>
+  </div>
+
+</div>
 <img src={bannerImg} alt="banner" className='header-logo' />'
 
    </header>

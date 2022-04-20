d9e9412fecbb0bd78ffe491f571f49e30f1bf253
Virenk21 <viren.d222111@gmail.com>
2022-04-21 03:30:18 +0530

    updated logocss

--- src/components/Common/Logo/index.jsx
+++ src/components/Common/Logo/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './style.css';
+import logoImg from './logofdapp.png'
 
 const Logo = () => {
   return (
-    <div>
-        <p>Logo</p>
+    <div className='header-content_logo'>
+        <div>
+          <img src={logoImg} alt="logo" />
+          <span>
+        <b>Taste</b>
+        </span>
+         <p>
+      <b>Restaurant & BBQ</b>
+    </p>
+        </div>
     </div>
   )
 }

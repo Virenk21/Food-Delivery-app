27ee3673daad637f0105d25864d0b018eb3d97a0
Virenk21 <viren.d222111@gmail.com>
2022-04-21 04:11:04 +0530

    menu updated with detailed items

---
+++ src/components/Common/Menu/MenuItems/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const MenuItem = ({item}) => {
+    const { id, name, info, price, img }=item
+  return (
+    <div className='item'>
+        <img src={img} alt="food" />
+        <div className='item-head_desc'>
+        <p className='head_desc-name'>{name}</p>
+        <p className='head_desc-info'>
+          <small>{info}</small>
+        </p>
+    </div>
+    <div className='item-foot_desc'>
+        <span className='foot_desc-price'>₹{price}</span>
+        <button>Add +</button>
+    </div>
+
+    </div>
+
+  )
+}
+
+export default MenuItem
--- src/components/Common/Menu/index.jsx
+++ src/components/Common/Menu/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { menuItemsData } from './data';
+import MenuItem from './MenuItems';
 import './style.css';
 const Menu = () => {
   return (
    <main>
     {menuItemsData.map((item) => (
-      <p>{item.name}</p>
+      <MenuItem item={item} />
     ))}
 
    </main>

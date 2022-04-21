8219cd2d099c9a93227998372bec853533ecb84d
Virenk21 <viren.d222111@gmail.com>
2022-04-21 06:07:20 +0530

    cartpage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import React from 'react';
 import './App.css';
 import Homepage from './Pages/Homepage';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Cart from './Pages/Cart';
 
-function App() {
+const App = () => {
   return (
-    <>
-    <div className='container'> 
-    < Homepage />
-    </div>
-    </>
+    <div className='container'>
+      <Router>
+        <Routes>
+         <Route exact path="/" element={<Homepage/>}/>
+          <Route exact path="/Cart" element={<Cart/>}/>
+        </Routes>
+      </Router>
+      </div>
   );
 }
 
---
+++ src/Pages/Cart/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <>CRTPAGE</>
+  )
+}
+
+export default Cart

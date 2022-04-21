9be8ba95f460d294b87aac3f4a045bc8624d8a77
Virenk21 <viren.d222111@gmail.com>
2022-04-21 06:16:52 +0530

    added routing

--- src/components/Common/CartCountButton/index.jsx
+++ src/components/Common/CartCountButton/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Navigate, unstable_HistoryRouter, useNavigate } from 'react-router-dom'
 import './style.css'
 
 const CartCountButton = ({cartCount}) => {
+  const navigate = useNavigate();
   return (
-    <div className='btnCartCount'>
+    <div className='btnCartCount' onClick={() =>navigate('/cart')}>
       <div className='count'>{cartCount >= 100 ? '99+' : cartCount}</div>
        <i class='fas fa-shopping-cart'></i>
 

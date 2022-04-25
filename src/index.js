a4b72da9530c57c6580f936c13a7fecd9e1707f6
Virenk21 <viren.d222111@gmail.com>
2022-04-25 23:19:57 +0530

    updated storejs

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
 
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      
       <App />
+    
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,7 @@
+import { createstore, applyMiddleware, compose } from 'redux';
 import { configureStore } from "@reduxjs/toolkit";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = configureStore({
   reducer: {},
